c70f801313f0536dd13f6abc1a16c9244063fa9a
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2024-01-10 00:48:17 +0200

    edit error: 8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,11 @@
 function cleanSet(set, startString) {
-  const filteredValues = [...set].filter((value) => value.startsWith(startString));
-  return filteredValues.join(startString).slice(startString.length);
+  if (!set || !startString || !(set instanceof Set) || typeof startString !== 'string' || startString === '' || set.size === 0) {
+    return '';
+  }
+  const filteredValues = Array.from(set).filter((value) => value.startsWith(startString));
+  const cleanedValues = filteredValues.map((value) => value.slice(startString.length));
+  const nonEmptyValues = cleanedValues.filter((value) => value !== '');
+  return nonEmptyValues.join('-');
 }
 
 export default cleanSet;
