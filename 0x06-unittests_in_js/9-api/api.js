3b8fe463bd5b0b05ee0f2ad5c2e22a721d5f6cd2
EslamAlawamy <125466790+EslamAlawamy@users.noreply.github.com>
2024-03-01 22:56:08 +0200

    Update api.js

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -1,19 +1,20 @@
 const express = require('express');
 
 const app = express();
+const PORT = 7865;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Welcome to the payment system');
 });
 
-app.get('/cart/:id', (req, res) => {
-  if (isNaN(req.params.id)) {
-    // raise a 404 error
-    res.sendStatus(404);
-  } else {
-    res.send(`Payment methods for cart ${req.params.id}`);
-  }
+app.get('/cart/:id(\\d+)', (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
 });
-app.listen(7865, () => {
-  console.log('API available on localhost port 7865');
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
 });
+
+module.exports = app;
